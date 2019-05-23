ac8e2e04090eccd91b658207e713ad026491a67c
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2019-05-23 16:38:00 +0200

    Inlining test.

--- src/Bench.elm
+++ src/Bench.elm
@@ -15,26 +15,98 @@ suite =
     Benchmark.describe "Codegen experiments"
         [ Benchmark.describe "Reverse"
             [ benchmark "ints" <|
-                \_ -> List.reverse intList
+                \_ -> reverse intList
             , benchmark "strings" <|
-                \_ -> List.reverse stringList
+                \_ -> reverse stringList
             ]
         , Benchmark.describe "ReduceLeft"
             [ benchmark "int +" <|
-                \_ -> List.foldl (+) 0 intList
+                \_ -> foldl (+) 0 intList
             , benchmark "string ++" <|
-                \_ -> List.foldl (++) "" stringList
+                \_ -> foldl (++) "" stringList
             ]
         , Benchmark.describe "Map"
             [ benchmark "* 2" <|
-                \_ -> List.map (\a -> a * 2) intList
+                \_ -> map (\a -> a * 2) intList
             , benchmark "String reverse" <|
-                \_ -> List.map String.reverse stringList
+                \_ -> map String.reverse stringList
             ]
         , Benchmark.describe "Filter"
             [ benchmark "isEven" <|
-                \_ -> List.filter (\a -> modBy 2 a == 0) intList
+                \_ -> filter (\a -> modBy 2 a == 0) intList
             , benchmark "String.endsWith 2" <|
-                \_ -> List.filter (\a -> String.endsWith "2" a) stringList
+                \_ -> filter (\a -> String.endsWith "2" a) stringList
             ]
         ]
+
+
+filter : (a -> Bool) -> List a -> List a
+filter isGood list =
+    foldr
+        (\x xs ->
+            if isGood x then
+                x :: xs
+
+            else
+                xs
+        )
+        []
+        list
+
+
+map : (a -> b) -> List a -> List b
+map f xs =
+    foldr (\x acc -> f x :: acc) [] xs
+
+
+foldl : (a -> b -> b) -> b -> List a -> b
+foldl func acc list =
+    case list of
+        [] ->
+            acc
+
+        x :: xs ->
+            foldl func (func x acc) xs
+
+
+foldr : (a -> b -> b) -> b -> List a -> b
+foldr fn acc ls =
+    foldrHelper fn acc 0 ls
+
+
+foldrHelper : (a -> b -> b) -> b -> Int -> List a -> b
+foldrHelper fn acc ctr ls =
+    case ls of
+        [] ->
+            acc
+
+        a :: r1 ->
+            case r1 of
+                [] ->
+                    fn a acc
+
+                b :: r2 ->
+                    case r2 of
+                        [] ->
+                            fn a (fn b acc)
+
+                        c :: r3 ->
+                            case r3 of
+                                [] ->
+                                    fn a (fn b (fn c acc))
+
+                                d :: r4 ->
+                                    let
+                                        res =
+                                            if ctr > 500 then
+                                                foldl fn acc (reverse r4)
+
+                                            else
+                                                foldrHelper fn acc (ctr + 1) r4
+                                    in
+                                    fn a (fn b (fn c (fn d res)))
+
+
+reverse : List a -> List a
+reverse list =
+    foldl (::) [] list
